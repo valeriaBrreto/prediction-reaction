faf31f6fef7a6351ae7859a215ec1eed42503564
Jc-Arroyo <jcarroyos@gmail.com>
2024-09-28 00:25:36 +0000

    update logic

--- docs/reaction.js
+++ docs/reaction.js
@@ -1,8 +1,21 @@
+function getReactionText(prediction) {
+    const isFire = prediction[2].probability >= 0.50;
+    const othersAboveThreshold = prediction.some((pred, index) => index !== 2 && pred.probability > 0.50);
+
+    if (isFire && !othersAboveThreshold) {
+        return "AUXILIO FUEGO";
+    } else if (!isFire && othersAboveThreshold) {
+        return "UFFF TODO TRANQUI";
+    }
+    return "SIN DATOS RELEVANTES";
+}
+
 function reaction2prediction(prediction) {
     const fireElement = document.getElementById("txt-reaction");
-    if (prediction[0].probability >= 0.50) {
-        fireElement.textContent = "AUXILIO FUEGO";
-    } else {
-        fireElement.textContent = "UFFF TODO TRANQUI";
-    }
+    fireElement.textContent = getReactionText(prediction);
+
+    const bodybg = document.getElementsByTagName("body")[0];
+    bodybg.style.backgroundColor = prediction[2].probability >= 0.50 ? "red" : "unset";
+
+    fireElement.textContent = getReactionText(prediction);
 }
